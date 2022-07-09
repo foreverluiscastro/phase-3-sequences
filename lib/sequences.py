b655c673ae748e30ec917dd0abc21f77fddaa815
Luis <foreverluiscastro@gmail.com>
2022-07-09 18:26:52 -0400

    Done.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def print_fibonacci(length):
     pass
+    if length == 0:
+        return []
+    else:
+        sequenced_list = []
+        for n in range(length):
+            if n == 0 or n == 1:
+                sequenced_list.append(n)
+                print(n)
+            else:
+                next_squence = sequenced_list[n-2] + sequenced_list[n-1]
+                sequenced_list.append(next_squence)
+                print(next_squence)
